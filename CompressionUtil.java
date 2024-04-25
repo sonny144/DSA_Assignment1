3638e9ce02dd52458cbefb5ac3317dd66b851ed5
Natangue123rza <145107578+Natangue123rza@users.noreply.github.com>
2024-04-25 21:56:19 +0200

    Create CompressionUtil.java

---
+++ CompressionUtil.java
@@ -0,0 +1,31 @@
+public class CompressionUtil {
+
+    // Compresses  string using RLE
+    public static byte[] compress(String text) {
+        StringBuilder compressed = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            int count = 1;
+            while (i + 1 < text.length() && text.charAt(i) == text.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            compressed.append(text.charAt(i));
+            compressed.append(count);
+        }
+        return compressed.toString().getBytes();
+    }
+
+    // Decompresses a string using RLE
+    public static String decompress(byte[] compressedBytes) {
+        String compressedString = new String(compressedBytes);
+        StringBuilder decompressed = new StringBuilder();
+        for (int i = 0; i < compressedString.length(); i += 2) {
+            char character = compressedString.charAt(i);
+            int count = Character.getNumericValue(compressedString.charAt(i + 1));
+            while (count-- > 0) {
+                decompressed.append(character);
+            }
+        }
+        return decompressed.toString();
+    }
+}
