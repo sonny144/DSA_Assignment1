c7d3d15e97644679652566897d17368c1dec73e8
Bathromeus <132193017+Bathromeus@users.noreply.github.com>
2024-04-25 22:48:55 +0200

    Update ClientNode.java

--- ClientNode.java
+++ ClientNode.java
@@ -16,19 +16,36 @@ public class ClientNode {
     }
 
     // Receive method
-    public void receive(String message) {
-        messages.add("Received: " + message);
-    }
-
-    // Display received messages
-    public void displayMessages() {
-        for (String message : messages) {
-            System.out.println(message);
-        }
+    public void receive(String message, String senderID) {
+        // Print the received message along with the sender's ID
+        System.out.println("Received from " + senderID + ": " + message);
     }
 
     // Get the ID of the client node
     public String getID() {
         return ID;
+    }
+}
+
+// Main class to demonstrate the functionality
+public class StarTopologyDemo {
+    public static void main(String[] args) {
+        // Create a server node
+        ServerNode server = new ServerNode();
+        
+        // Create client nodes
+        ClientNode client1 = new ClientNode("Client1");
+        ClientNode client2 = new ClientNode("Client2");
+        ClientNode client3 = new ClientNode("Client3");
+        
+        // Add client nodes to the server
+        server.addClientNode(client1);
+        server.addClientNode(client2);
+        server.addClientNode(client3);
+        
+        // Send messages from client nodes
+        client1.send("Hello from Client1", server);
+        client2.send("Hi from Client2", server);
+        client3.send("Hey from Client3", server);
     }
 }
