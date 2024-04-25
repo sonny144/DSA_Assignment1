972645cd433cc10f7de05a5c61408ba73aaaa2d1
mi-cheal <116207443+mi-cheal@users.noreply.github.com>
2024-04-25 17:31:00 +0200

    Uses "server.unregisterClient" for deletion, and combines client naming with maintained message functionalities.

--- ClientNode.java
+++ ClientNode.java
@@ -2,33 +2,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientNode {
-    private String ID;
-    private List<String> messages;
+  private String clientName; // Use a more descriptive name
+  private List<String> messages;
 
-    public ClientNode(String ID) {
-        this.ID = ID;
-        messages = new ArrayList<>();
-    }
+  public ClientNode(String clientName) {
+    this.clientName = clientName;
+    messages = new ArrayList<>();
+  }
 
-    // Send method, wrapper around a call to server node
-    public void send(String message, ServerNode server) {
-        server.brokerMessage(message, ID);
-    }
+  // Send method, wrapper around a call to server node
+  public void send(String message, ServerNode server) {
+    server.sendMessage(message, this); // Use sendMessage for consistency
+  }
 
-    // Receive method
-    public void receive(String message) {
-        messages.add("Received: " + message);
-    }
+  // Receive method
+  public void receive(String message) {
+    messages.add("Received: " + message);
+  }
 
-    // Display received messages
-    public void displayMessages() {
-        for (String message : messages) {
-            System.out.println(message);
-        }
+  // Display received messages
+  public void displayMessages() {
+    for (String message : messages) {
+      System.out.println(message);
     }
+  }
 
-    // Get the ID of the client node
-    public String getID() {
-        return ID;
-    }
+  // Get the client name
+  public String getClientName() {
+    return clientName;
+  }
 }
--- Main.java
+++ Main.java
@@ -1,39 +1,39 @@
 public class Main {
-    public static void main(String[] args) {
-        // Create server node
-        ServerNode server = new ServerNode();
-        
-        // Create client nodes
-        ClientNode client1 = new ClientNode("Client1");
-        ClientNode client2 = new ClientNode("Client2");
-        ClientNode client3 = new ClientNode("Client3");
-        ClientNode client4 = new ClientNode("Client4");
-        
-        // Create star network
-        Star starNetwork = new Star();
-        
-        // Insert client nodes into the star network
-        starNetwork.insertNode(client1);
-        starNetwork.insertNode(client2);
-        starNetwork.insertNode(client3);
-        starNetwork.insertNode(client4);
-        
-        // Send messages between client nodes
-        client1.send("Hello from Client1", server);
-        client2.send("Hi from Client2", server);
-        client3.send("Greetings from Client3", server);
-        client3.send("Greetings from Client4, how is everyone?", server);
-        
-        // Display messages received by client nodes
-        client1.displayMessages();
-        client2.displayMessages();
-        client3.displayMessages();
-        client4.displayMessages();
-        
-        System.out.println("\nBefore deletion:");
-        starNetwork.displayClientNodes(); // Display client nodes before deletion
-        starNetwork.deleteNode(client3); // Delete client2
-        System.out.println("\nAfter deletion:");
-        starNetwork.displayClientNodes(); // Display client nodes after deletion
-    }
+
+  public static void main(String[] args) {
+    ServerNode server = new ServerNode();
+
+    ClientNode client1 = new ClientNode("Client1");
+    ClientNode client2 = new ClientNode("Client2");
+    ClientNode client3 = new ClientNode("Client3");
+    ClientNode client4 = new ClientNode("Client4");
+
+    // No need for Star class, directly manage clients
+
+    client1.send("Hello from Client1", server);
+    client2.send("Hi from Client2", server);
+    client3.send("Greetings from Client3", server);
+    client4.send("Greetings from Client4, how is everyone?", server);
+
+    // Add displayMessages method to ClientNode to show received messages
+    client1.displayMessages();
+    client2.displayMessages();
+    client3.displayMessages();
+    client4.displayMessages();
+
+    System.out.println("\nBefore deletion:");
+    System.out.println("Client 1: " + client1.getClientName());
+    System.out.println("Client 2: " + client2.getClientName());
+    System.out.println("Client 3: " + client3.getClientName());
+    System.out.println("Client 4: " + client4.getClientName());
+
+    System.out.println("\nAfter deletion:");
+    server.unregisterClient(client3); // Use server to unregister client
+    System.out.println("Client 1: " + client1.getClientName());
+    System.out.println("Client 2: " + client2.getClientName());
+
+    
+    // Client3 is unregistered, so it won't be displayed
+    System.out.println("Client 4: " + client4.getClientName());
+  }
 }
