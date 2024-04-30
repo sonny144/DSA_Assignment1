e32e1278e013fd753077c201ac1aa14b4556fd45
sonny144 <marknyambe14@gmail.com>
2024-04-30 22:39:11 +0200

    main update

--- Main.java
+++ Main.java
@@ -1,32 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        ServerNode server = new ServerNode();
+        ServerNode server = new ServerNode(); //creates instance of server node class. It represents the server node in the network
         
-        ClientNode client1 = new ClientNode("Client1");
+        ClientNode client1 = new ClientNode("Client1"); //creates instance of client nodes with their own unique identifier
         ClientNode client2 = new ClientNode("Client2");
         ClientNode client3 = new ClientNode("Client3");
         ClientNode client4 = new ClientNode("Client4");
         ClientNode client5 = new ClientNode("Client5");
 
-        Star starNetwork = new Star();
-        starNetwork.insertNode(client1);
+        Star starNetwork = new Star(); //creates instance of Star class
+        starNetwork.insertNode(client1); //calls the insert method to add clients
         starNetwork.insertNode(client2);
         starNetwork.insertNode(client3);
         starNetwork.insertNode(client4);
         starNetwork.insertNode(client5);
 
-        // Send messages between client nodes
-        client1.send("Hello from Client1", server,client3);
-        client2.send("Hi from Client2", server,client1);
-        client3.send("Greetings from Client3", server,client2);
-        client4.send("Greetings from Client4, how is everyone?", server,client5);
+        // Sending of messages between clients
+        client1.send("Hello from Client1", server,client3); //sends message from client1 to client3
+        client2.send("Hi from Client2", server,client1);// sends message from client2 to client1
+        client3.send("Greetings from Client3", server,client2); //sends message from client3 to client2
+        client4.send("Greetings from Client4, how is everyone?", server,client5); //sends message from client4 to client5
 
 
-        // Display client nodes before deletion
-        System.out.println("\nBefore deletion:");
+        System.out.println("\nBefore deletion:");  //displays state before deletion occurs
         starNetwork.displayClientNodes();
         
-        // Delete a client node and show the state after deletion
+        // Diplays state after any deletion that might occur
         //starNetwork.deleteNode(client1);
         System.out.println("\nAfter deletion:");
         starNetwork.displayClientNodes();
