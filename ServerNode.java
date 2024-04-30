489fb85ebf1d33524743fcfe22a1fefe4c1beafe
sonny144 <marknyambe14@gmail.com>
2024-04-30 22:18:38 +0200

    server update3

--- ServerNode.java
+++ ServerNode.java
@@ -1,33 +1,28 @@
 import java.util.ArrayList;
 import java.util.List;
 
-//This class represents the Server Node in the star topology network
 public class ServerNode {
+    private List<ClientNode> clientNodes = new ArrayList<>();
 
-    //This list stores all the client nodes connected to the server node
-    private List<ClientNode> clientNodes;
-    
-    //Constructor to initialize the server node
-    public ServerNode() {
-        //Initialize the list of client nodes
-        clientNodes = new ArrayList<>();
-    }
-
-    //Method to add a client node to the server's list of client nodes
     public void addClientNode(ClientNode node) {
-        //Add the client node to the list of client nodes
         clientNodes.add(node);
     }
 
-    //Method to broker a message sent by a client node
-    public void brokerMessage(String message, String senderID) {
-        // Send the message to all client nodes except the sender
+    public void brokerMessage(byte[] compressedMessage, String senderID) {
+        // Decompressing the message to log it, then recompress for sending
+        String message = CompressionUtil.decompress(compressedMessage);
+        System.out.println("Server is brokering a message from " + senderID + ": \"" + message + "\"");
+
+        // Recompress message for sending to other clients
+        byte[] recompressedMessage = CompressionUtil.compress(message);
+
         for (ClientNode node : clientNodes) {
-            // Check if the current client node is not the sender
             if (!node.getID().equals(senderID)) {
-                // Send the message to the client node
-                node.receive(message, senderID);
+                System.out.println("Server sending message to " + node.getID());
+                node.receive(recompressedMessage, senderID);
             }
         }
     }
 }
+
+
