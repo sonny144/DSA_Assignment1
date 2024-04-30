7282f5c498fc9b30266850aa822a96ee46eea703
sonny144 <marknyambe14@gmail.com>
2024-04-30 21:54:42 +0200

    main change

--- Main.java
+++ Main.java
@@ -6,31 +6,29 @@ public class Main {
         ClientNode client2 = new ClientNode("Client2");
         ClientNode client3 = new ClientNode("Client3");
         ClientNode client4 = new ClientNode("Client4");
+        ClientNode client5 = new ClientNode("Client5");
 
         Star starNetwork = new Star(); //creates instance of Star class to represent the star topology
         starNetwork.insertNode(client1); //we use the insertNode method to add clients to the star topology
         starNetwork.insertNode(client2);
         starNetwork.insertNode(client3);
         starNetwork.insertNode(client4);
+        starNetwork.insertNode(client5);
 
         // Messages are sent from each client to the servernode using the send method from the ClientNode class
-        client1.send("Hello from Client1", server);
-        client2.send("Hi from Client2", server);
-        client3.send("Greetings from Client3", server);
-        client4.send("Greetings from Client4, how is everyone?", server);
+        client1.send("Hello from Client1", server,client2);
+        client2.send("Hi from Client2", server,client3);
+        client3.send("Greetings from Client3", server,client4);
+        client4.send("Greetings from Client4, how is everyone?", server,client5);
 
-        // Messages from the client nodes are displayed using the .displayMessages method
-        client1.displayMessages();
-        client2.displayMessages();
-        client3.displayMessages();
-        client4.displayMessages();
+        
 
-        // We use this to display the current nodes that exist before deletion
+        // We use this to display the current nodes that exist before deletion if
         System.out.println("\nBefore deletion:");
         starNetwork.displayClientNodes();
         
-        // After deleting a specific node we then also print out the current nodes that exist after deletion
-        starNetwork.deleteNode(client3);
+        // After deleting a specific node we then also print out the current nodes that exist if a node is deleted
+        //starNetwork.deleteNode(client3);
         System.out.println("\nAfter deletion:");
         starNetwork.displayClientNodes();
     }
