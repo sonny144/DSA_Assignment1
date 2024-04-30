b63f7d4c2e077f317f5f5f6d8fb5a5091cb06fbd
sonny144 <marknyambe14@gmail.com>
2024-04-30 21:55:33 +0200

    client update2

--- ClientNode.java
+++ ClientNode.java
@@ -10,27 +10,17 @@ public class ClientNode { // this represents a client Node which can send and re
         messages = new ArrayList<>(); //we initialize the list for the messages
     }
 
-    public void send(String message, ServerNode server) { // send method takes in the message to be sent and also the server node to which the message is being sent
+    public void send(String message, ServerNode server,ClientNode recipient) { // send method takes in the message to be sent and also the server node to which the message is being sent
         System.out.println(this.ID + " sending message: \"" + message + "\"");
         byte[] compressedMessage = CompressionUtil.compress(message); //compress the message
-        server.brokerMessage(compressedMessage, this.ID); // delivers messahe to the server
+        server.brokerMessage(compressedMessage, this.ID); // delivers message to the server
+        recipient.receive(compressedMessage,this.ID); // message is received by the particular clientNode
     }
 
     public void receive(byte[] compressedMessage, String senderID) { // method to receive message from another node
         String message = CompressionUtil.decompress(compressedMessage); //decompress the message
         messages.add("Received from " + senderID + ": \"" + message + "\"");
         System.out.println(this.ID + " received message from " + senderID + ": \"" + message + "\"");
-    }
-
-    public void displayMessages() { //method to display all the messages that are sent
-        if (messages.isEmpty()) {
-            System.out.println(this.ID + " has no messages."); 
-        } else {
-            System.out.println(this.ID + " messages:"); // in situation where the there is no message
-            for (String msg : messages) {
-                System.out.println(msg);
-            }
-        }
     }
 
     public String getID() {
