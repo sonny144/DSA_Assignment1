30f51c993ca61fd9fee572d6f2f4ad3bc548b038
sonny144 <marknyambe14@gmail.com>
2024-04-30 20:02:16 +0200

    client update

--- ClientNode.java
+++ ClientNode.java
@@ -1,51 +1,39 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
-public class ClientNode {
-    private String ID;
-    private List<String> messages;
+public class ClientNode { // this represents a client Node which can send and receive messages
+    private String ID; // unique identifier for each client
+    private List<String> messages; //list that will store the messages from the clients
 
-    public ClientNode(String ID) {
+    public ClientNode(String ID) { // constructor the clientNode which initializes new instance of ClientNode with its unique ID. 
         this.ID = ID;
-        messages = new ArrayList<>();
+        messages = new ArrayList<>(); //we initialize the list for the messages
     }
 
-    // Send method, wrapper around a call to server node
-    public void send(String message, ServerNode server) {
-        server.brokerMessage(message, ID);
+    public void send(String message, ServerNode server) { // send method takes in the message to be sent and also the server node to which the message is being sent
+        System.out.println(this.ID + " sending message: \"" + message + "\"");
+        byte[] compressedMessage = CompressionUtil.compress(message); //compress the message
+        server.brokerMessage(compressedMessage, this.ID); // delivers messahe to the server
     }
 
-    // Receive method
-    public void receive(String message, String senderID) {
-        // Print the received message along with the sender's ID
-        System.out.println("Received from " + senderID + ": " + message);
+    public void receive(byte[] compressedMessage, String senderID) { // method to receive message from another node
+        String message = CompressionUtil.decompress(compressedMessage); //decompress the message
+        messages.add("Received from " + senderID + ": \"" + message + "\"");
+        System.out.println(this.ID + " received message from " + senderID + ": \"" + message + "\"");
     }
 
-    // Get the ID of the client node
+    public void displayMessages() { //method to display all the messages that are sent
+        if (messages.isEmpty()) {
+            System.out.println(this.ID + " has no messages."); 
+        } else {
+            System.out.println(this.ID + " messages:"); // in situation where the there is no message
+            for (String msg : messages) {
+                System.out.println(msg);
+            }
+        }
+    }
+
     public String getID() {
-        return ID;
-    }
-}
-
-// Main class to demonstrate the functionality
-public class StarTopologyDemo {
-    public static void main(String[] args) {
-        // Create a server node
-        ServerNode server = new ServerNode();
-        
-        // Create client nodes
-        ClientNode client1 = new ClientNode("Client1");
-        ClientNode client2 = new ClientNode("Client2");
-        ClientNode client3 = new ClientNode("Client3");
-        
-        // Add client nodes to the server
-        server.addClientNode(client1);
-        server.addClientNode(client2);
-        server.addClientNode(client3);
-        
-        // Send messages from client nodes
-        client1.send("Hello from Client1", server);
-        client2.send("Hi from Client2", server);
-        client3.send("Hey from Client3", server);
+        return ID;  // returns unique identifier for the clients
     }
 }
