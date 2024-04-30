634df502310f59975a4169f9819f397a04587bc3
sonny144 <marknyambe14@gmail.com>
2024-04-30 22:18:06 +0200

    chang4

--- ClientNode.java
+++ ClientNode.java
@@ -1,29 +1,30 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class ClientNode { // this represents a client Node which can send and receive messages
-    private String ID; // unique identifier for each client
-    private List<String> messages; //list that will store the messages from the clients
+public class ClientNode {
+    private String ID;
+    private List<String> messages;
 
-    public ClientNode(String ID) { // constructor the clientNode which initializes new instance of ClientNode with its unique ID. 
+    public ClientNode(String ID) {
         this.ID = ID;
-        messages = new ArrayList<>(); //we initialize the list for the messages
+        messages = new ArrayList<>();
     }
 
-    public void send(String message, ServerNode server,ClientNode recipient) { // send method takes in the message to be sent server and client node to which the message is being sent
+    public void send(String message, ServerNode server,ClientNode recipient) {
         System.out.println(this.ID + " sending message: \"" + message + "\"");
-        byte[] compressedMessage = CompressionUtil.compress(message); //compress the message
-        server.brokerMessage(compressedMessage, this.ID); // delivers message to the server
-        recipient.receive(compressedMessage,this.ID); // message is received by the particular clientNode
+        byte[] compressedMessage = CompressionUtil.compress(message);
+        server.brokerMessage(compressedMessage, this.ID);
+        recipient.receive(compressedMessage,this.ID);
     }
-
-    public void receive(byte[] compressedMessage, String senderID) { // method to receive message from another node
-        String message = CompressionUtil.decompress(compressedMessage); //decompress the message
+   
+    public void receive(byte[] compressedMessage, String senderID) {
+        String message = CompressionUtil.decompress(compressedMessage);
         messages.add("Received from " + senderID + ": \"" + message + "\"");
         System.out.println(this.ID + " received message from " + senderID + ": \"" + message + "\"");
     }
+    
 
     public String getID() {
-        return ID;  // returns unique identifier for the clients
+        return ID;
     }
 }
